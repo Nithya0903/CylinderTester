a97974ba295962fd83aee02efde3307ca6652c9c
Nithya <nithyamanoj.ms@gmail.com>
2022-02-23 20:52:27 +0530

    database cleared

--- public/Main.js
+++ public/Main.js
@@ -29,11 +29,11 @@ const createWindow = () => {
 
     
     // In development mode, if the window has loaded, then load the dev tools.
-    if (isDev) {
-        mainWindow.webContents.on('did-frame-finish-load', () => {
-            mainWindow.webContents.openDevTools({ mode: 'detach' });
-        });
-    }
+    // if (isDev) {
+    //     mainWindow.webContents.on('did-frame-finish-load', () => {
+    //         mainWindow.webContents.openDevTools({ mode: 'detach' });
+    //     });
+    // }
 };
 
 app.setPath(
